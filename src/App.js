b928133c7dcf37bd3174ddd1617987588a880ef7
gopikriss <gopikrishna.web93@gmail.com>
2023-08-23 19:48:34 +0530

    new container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navbar } from './components';
-import { AboutUs, Chef, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { AboutUs, Chef, FindUs, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
 import './App.css';
 
 function App() {
@@ -14,6 +14,7 @@ function App() {
       <Intro />
       <Laurels />
       <Gallery />
+      <FindUs />
     </div>
   );
 }
--- src/container/index.js
+++ src/container/index.js
@@ -5,6 +5,7 @@ import { default as Chef } from './Chef/Chef';
 import { default as Intro } from './Intro/Intro';
 import { default as Laurels } from './Laurels/Laurels';
 import { default as Gallery } from './Gallery/Gallery';
+import { default as FindUs } from './FindUs/FindUS';
 
 export {
     Header,
@@ -14,4 +15,5 @@ export {
     Intro,
     Laurels,
     Gallery,
+    FindUs,
 }
